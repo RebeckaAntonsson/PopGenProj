2ddd0192070639ec7edb322c6345e0f5647cbeba
Rebecka <rebeckabex.antonsson@gmail.com>
2025-03-11 11:56:42 +0100

    The haplogroup app script now only loads the needed columns from the AADR csv, uses encoding ISO-8859-1 (bc otherwise there is some touble), currently printing the head of a mtDNA haplogroup list. Next steps would be to filter the list since many rows are empty and therefore not useful.

--- haplogroup_app.py
+++ haplogroup_app.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
-import streamlit as st
+#import streamlit as st
 import pandas as pd
 
-st.title("Haplogroup")
 
-haplogroup = st.text_input("Enter Haplogroup (e.g., R1b, H1):")
+#st.title("Haplogroup")
+#haplogroup = st.text_input("Enter Haplogroup (e.g., R1b, H1):")
+
+
+# Loads the AADR csv file into a pandas data frame
+def load_AADR_data():
+    AADR_df = pd.read_csv("AADR_Annotation(Sheet1).csv", sep=';', encoding='ISO-8859-1', usecols=[0,8,13,14,28])
+    return AADR_df
+
+# Function that matches the users input haplotype to the list with hapogroups
+#def match_haplogroup(haplotype, haplogroup_list):
+    
+    
+    
+AADR_df = load_AADR_data()
+haplogroup_list=AADR_df["mtDNA haplogroup if >2x or published"]
+print(haplogroup_list.head())
