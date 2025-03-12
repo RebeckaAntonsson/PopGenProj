adf0c86aec8d1bc02aa63cf0c46531ecfac2ef99
Rebecka <rebeckabex.antonsson@gmail.com>
2025-03-12 11:21:10 +0100

    Added a test excel file to use, and the haplogroup script can now read an excel file instead of csv (the csv file got some weird columns so had to change

--- haplogroup_app.py
+++ haplogroup_app.py
@@ -10,8 +10,7 @@ def load_AADR_data():
     #For the real deal
     #AADR_df = pd.read_excel("AADR Annotations 2025.xlsx")
     # for testing
-    AADR_df = pd.read_excel("test_AADR.xlsx", engine="openpyxl")
-    
+    AADR_df = pd.read_excel("test_AADR.xlsx", engine="openpyxl", usecols=[0,8,13,14,28])
     
     return AADR_df
 
@@ -26,24 +25,27 @@ def match_haplogroup(haplotype, haplogroup_list):
     
     
 AADR_df = load_AADR_data()
-print(AADR_df.head())
-print(AADR_df.columns)
+
 # Some rows does not have any mtDNA haplogroup information, these rows will 
-# just say "..". Here the rows without haplgroup information are removed.
-#filtered_AADR_df = AADR_df[AADR_df["mtDNA haplogroup if >2x or published"] != ".."]
-# Also (or maybe change to removing n/a (<2x))
-#print(filtered_AADR_df.head())
+# say "n/a". Here the rows without haplgroup information are removed.
+# The code says "if the column contains n/a", but the ~before AADR is an
+# invert operatior for boolean. So the output is everything that does not
+# match the pattern ("n/a")
+filtered_AADR_df = AADR_df[~AADR_df["mtDNA haplogroup if >2x or published"].str.contains("n/a")]
 
 # Create list with only the haplotypes, from the AADR filtered dataframe
-#haplogroup_list=filtered_AADR_df["mtDNA haplogroup if >2x or published"]
+haplogroup_list=filtered_AADR_df["mtDNA haplogroup if >2x or published"]
 
+# Ask user to input their hapotype and save it in the hapotype variable
 #st.title("Haplogroup")
 #haplogroup = st.text_input("Enter Haplogroup (e.g., R1b, H1): ")
-#haplotype = input("Enter your haplotype (e.g., R1b, H1): ")
+haplotype = input("Enter your haplotype for example R1b, D4b1a2a. \nDO NOT enter entire files like no fasta, csv, json etc \nDO NOT enter only mutations for example H1a1 T16093C G16213A. \nPlease enter your haplotype here: ")
+print("Haplotype",haplotype)
 
 # Calls the function that matches users input with a haplogroup
-#matched_haplogroup = match_haplogroup(haplotype, haplogroup_list)
-#print(matched_haplogroup)
+matched_haplogroup = match_haplogroup(haplotype, haplogroup_list)
+print("Matched haplogroup", matched_haplogroup)
 
-#matching_row = filtered_AADR_df[filtered_AADR_df["mtDNA haplogroup if >2x or published"] == matched_haplogroup]
-#print(matching_row)
+# Get the rows that match to the haplogroup 
+matching_row = filtered_AADR_df[filtered_AADR_df["mtDNA haplogroup if >2x or published"] == matched_haplogroup]
+print(matching_row)
