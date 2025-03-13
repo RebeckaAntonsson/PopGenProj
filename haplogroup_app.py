eb5b17661b8c461e534f7c4bc587ca2396d85172
Rebecka <rebeckabex.antonsson@gmail.com>
2025-03-13 10:49:12 +0100

    Now reads the VIP file as well and has a function that matches the haplotype/group with a group from the VIP excel

--- haplogroup_app.py
+++ haplogroup_app.py
@@ -14,6 +14,11 @@ def load_AADR_data():
     
     return AADR_df
 
+def load_VIP_file():
+    VIP_df = pd.read_excel("VIPHaplogroups.xlsx")
+    
+    return VIP_df
+
 # Function that matches the users input haplotype to the list with hapogroups
 def match_haplogroup(haplotype, haplogroup_list):
     # Convert all haplogrpups to strings
@@ -22,12 +27,18 @@ def match_haplogroup(haplotype, haplogroup_list):
     matches = [hg for hg in haplogroup_list if haplotype.startswith(hg)]
     
     return max(matches, key=len) if matches else None
+
+# Function that matches the users haplogroup with a haplogroup for VIP's
+def match_VIP(matched_haplogroup, VIP_haplogroups_list):
+    matches = [hg for hg in VIP_haplogroups_list if matched_haplogroup.startswith(hg)]
     
+    return max(matches, key=len) if matches else None
     
 AADR_df = load_AADR_data()
+
 # Rename one of the very long column names
 AADR_df = AADR_df.rename(columns={"Date mean in BP in years before 1950 CE [OxCal mu for a direct radiocarbon date, and average of range for a contextual date]": "Years before 1950", })
-print(AADR_df["mtDNA haplogroup if >2x or published"].head())
+
 # Some rows does not have any mtDNA haplogroup information, these rows will 
 # say "n/a". Here the rows without haplgroup information are removed.
 # The code says "if the column contains n/a", but the ~before AADR is an
@@ -49,14 +60,19 @@ haplotype = st.text_input("""Enter your haplotype, for example like "D4b1a2a"
                   \nPlease enter your haplotype here: """)
 
 
+
 if haplotype:
     # Calls the function that matches users input with a haplogroup
     matched_haplogroup = match_haplogroup(haplotype, haplogroup_list)
-    st.write(matched_haplogroup)
+    
+    VIP_df = load_VIP_file()
+    VIP_haplogroups_list = VIP_df["mtDNA"]
+    matched_VIP_group = match_VIP(matched_haplogroup, VIP_haplogroups_list)
+    print(matched_VIP_group)
+    
     
     # Get the rows that match to the haplogroup 
     matching_rows = filtered_AADR_df[filtered_AADR_df["Haplogroup"] == matched_haplogroup]
-    
     # If there is more than one haplogroup match, get the oldest one
     oldest_match = matching_rows.nlargest(1,"Years before 1950")
     # Plus 75 since the age is years before 1950, and its 2025 now
@@ -69,4 +85,5 @@ if haplotype:
     st.write("You have matched with the haplogroup ", matched_haplogroup)
     st.write("Your haplogroup is from ", age_of_match, 
              " years ago, and originated from", oldest_match["Political Entity"].iloc[0])
+    st.write("Your haplogroup matches with the following VIP's")
 
