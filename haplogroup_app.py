292e075d42af08d7f092fb9ae91fe09b770cd444
RebeckaAntonsson <rebeckabex.antonsson@gmail.com>
2025-03-17 15:29:47 +0100

    More comments, description of functions, procedure, decisions made, cases the code can handle and date is added

--- haplogroup_app.py
+++ haplogroup_app.py
@@ -3,6 +3,7 @@
 """
 haplogroup_app.py
 
+
 Description: This script will open a webpage where the user can input their
 haplotype. A haplogroup, from where and when it originates plus VIP with sharing 
 this haplogroup will be printed on the webpage as results. 
@@ -10,24 +11,105 @@ this haplogroup will be printed on the webpage as results.
     
 User defined functions: 6
 
-load_AADR_data
-load_VIP_file
-match_haplogroup
-match_VIP
-output_haplogroup
-output_VIP
+1. load_AADR_data
+Loads specified columns from the AADR dataset into a pandas datafram.
+Renames some columns and filter out data that is not useful.
+
+2. load_VIP_file
+Loads the VIP excel file into a pandas dataframe, and renames all columns
+
+3. match_haplogroup
+As input this function gets the haplotype that the user inputs and a list
+with haplogroups. The haplogroup list is simply the "Haplotype" column
+from the AADR dataframe.
+The function finds the best mathcing haplogroup to the users haplotype with
+a for loop and a if statement. 
+It returns the best matching haplogroup. If there is no match the function
+returns "None".
+
+4. match_VIP
+Matches the users haplogroup with a haplogroup for VIP's.
+Uses the same methology as the function match_haplogroup.
+As input this function gets the matched_haplogroup from the function 
+match_haplogroup and a list with VIP haplogroups.
+The VIP haplogroup list is simply the "Haplotype" column from the VIP dataframe.
+It returns the best matching haplogroup. If there is no match the function
+returns "None".
+
+5. output_haplogroup
+Extracts the results for the haplogroup match that will be printed to the 
+user in the web interface.
+The function takes the filtered_AADR_df and matched_haplogroup as input.
+If there is more than one haplogroup match, for example in the instances where
+there are duplicated, the oldest match is extracted.
+The function also calculated how many years old this match is.
+It returns the oldest_match and age_of_match.
+
+6. output_VIP
+Extracts the results for the VIP haplogroup match. 
+Returns a dataframe containing all VIP's with a matching haplogroup which will
+be printed to the user in the web interface.
+
 
 Non-standard modules: pandas, streamlit
 
+
 Procedure:
     
-Input:
-Output:
+    Main code
     
+1. Initiating the web interface, print some initial information and user 
+for input. Program continues only if and when the user inputs something.
+
+2. Calls function that loads the AADR excel file.
+
+3. Creates a list with only haplogroups, from the AADR dataframe previously 
+loaded.
+
+4. Calls function that matches user input with a haplogroup.
+
+5. Controll step that prints error message if there are no matching haplogrups.
+     
+6. Calls function that produces the results for the haplogroup match.
+        
+7. Calls function that loads the VIP haplogroup excel file.
+
+8. Creates a list with only haplogroups from the VIP excel file.
+
+9. Calls function that finds a matching haplogroup in the VIP file.
+
+10. Calls function that created results for the VIP match.
+
+11. Create the results printed to the web interface, using streamlit.
+            
+
+Decisions made:
+    - Political entity is used as place of origin. 
+    The information for "locality" had confusing places for example "Raqefet Cave" 
+    therefore political entity gave a more user friendly output. A map with the 
+    coordinates was added to provide truthful output. 
+    - Assuming the oldest haplogroup in the AADR datset it in fact the acctual
+    oldest instance of this group. There could be older discoveries of this a
+    certain haplogroup that is not yet included in the AADR, which is not taken
+    into consideration here. 
+
+
+Cases the code can handle:
+    - If the user inputs non existing haplotypes an error message with 
+    instructions will appear.
+    - If there are not matching VIP's there will be a message to the user.
+    
+    
+Input (in the interface): haplotype
+Output (in the interface): haplogroup, when and where the haplogroup emerged,
+a map showing location of emerge, and VIP's that shares the haplogroup.
+    
+
 Usage: streamlit run haplogroup.py
 
+
 Version: 1.00
-Date:
+Date: 17-03-2025
 Name: Rebecka Antonsson
 
 """
@@ -39,8 +121,17 @@ import pandas as pd
 
 # Loads the AADR excel file into a pandas data frame
 def load_AADR_data():
-    #For the real deal
-    AADR_df = pd.read_excel("AADR_Annotations_2025.xlsx", engine="openpyxl", usecols=[8,13,14,15,16,28])
+    # Loads the data set using pandas read excel function
+    """ Only 6 of the columns from the dataset is loaded with usecols=[8,13,14,15,16,28]
+    The following columns are loaded:
+        8: "Date mean in BP in years before 1950 CE [OxCal mu for a direct radiocarbon date, and average of range for a contextual date]"
+        14: "Political Entity"
+        15: "Lat."
+        16: "Long."
+        28: "mtDNA haplogroup if >2x or published"
+        """
+        
+    AADR_df = pd.read_excel("AADR_Annotations_2025.xlsx", engine="openpyxl", usecols=[8,14,15,16,28])
     
     # Rename one of the very long column names plus latidude and longitude
     # so the map function can understand the columns
@@ -104,7 +195,7 @@ def output_haplogroup(filtered_AADR_df, matched_haplogroup):
     # If there is more than one haplogroup match, get the oldest one
     oldest_match = matching_rows.nlargest(1,"Years before 1950")
     
-    # Plus 75 since the age is years before 1950, and its 2025 now
+    # Plus 75 since the age is years before 1950, and it's 2025 now
     age_of_match = oldest_match["Years before 1950"].iloc[0]
     age_of_match = int(age_of_match) + 75
     
@@ -112,8 +203,7 @@ def output_haplogroup(filtered_AADR_df, matched_haplogroup):
     return oldest_match, age_of_match
 
 
-# Function that filters out the correct output and writes the output results,
-# For the VIP output
+# Function that filters out the correct output for the VIP output
 def output_VIP(VIP_df, matched_VIP_group):
     
     # Gets all rows from the VIP file that matches with the Haplogroup
@@ -144,7 +234,7 @@ def main():
         # Call function that loads the AADR excel file, containing haplogroup information
         filtered_AADR_df = load_AADR_data()
         
-        # Create list with only the haplotypes, from the AADR filtered dataframe
+        # Create list with only the haplogroups, from the AADR filtered dataframe
         haplogroup_list=filtered_AADR_df["Haplogroup"]
         
         # Calls the function that matches users input with a haplogroup
